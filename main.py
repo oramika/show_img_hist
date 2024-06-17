a8fdc387aefbb360d2ebeff7a693ae3ad1b21b59
Hibiki Inaba <h.inaba.892@stn.nitech.ac.jp>
2024-06-17 20:56:16 +0900

    change caption message

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_image():
 
     st.image(
         im,
-        caption='image from wikimedia commons. This sentence is added.',
+        caption='image from wikimedia commons. I add this sentences.',
         use_column_width=True
     )
 
